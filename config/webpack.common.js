6519894064c9c6c60042fcacd474a6f4536330d7
kaiming.gu_sx <kaiming.gu_sx@aispeech.com>
2021-05-25 17:51:50 +0800

    Terser学习

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -8,18 +8,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { DefinePlugin } = require('webpack')
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
-const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const commonConfig = {
+const commonConfig =(isProduction)=> ({
     devtool: "source-map",      //使用source-map将打包后的文件映射生成原文件，方便调式和查看
-    context:path.resolve(__dirname,"../"),    //默认是webpack启动目录，现在这样设置是跟目录下
-    entry:  './src/main.js',  //入口 相对于context
+    context: path.resolve(__dirname, "../"),    //默认是webpack启动目录，现在这样设置是跟目录下
+    entry: {
+        main: './src/main.js',     //分包
+        main2: './src/main2.js',
+    },//入口 相对于context
+
     output: {
-        filename: "bundle.js",
+        filename: "[name].bundle.js",     //分包后的名字
         path: resolveApp("./dist"),   //出口:需要绝对路径
-        publicPath: "./"    //设置为"/"那么浏览器会根据所在的域名+路径去请求对应的资源,开发时设置为"./"以根据相对路径去查找资源(比如script引入js资源)
+        publicPath: "./",   //设置为"/"那么浏览器会根据所在的域名+路径去请求对应的资源,开发时设置为"./"以根据相对路径去查找资源(比如script引入js资源)
+        chunkFilename: "[name].[hash:6].chunk.js" //动态导入独立打包的文件名，name默认与id一致，可以通过magic comments（魔法注释）修改name
     },
-    
+
     resolve: {
         extensions: ['.wasm', '.mjs', '.js', '.json', '.jsx', '.ts', '.vue'],   //不写文件的扩展名，会在这个数组中匹配扩展名
         mainFiles: ['index'],    //不写文件夹名，会在这个数组中匹配文件夹名
@@ -28,11 +34,54 @@ const commonConfig = {
             "pages": resolveApp("./src/pages")
         }
     },
+
+    optimization: {
+        // 对代码进行压缩相关的操作
+        minimizer: [
+            new TerserPlugin({
+                extractComments: false,
+            }),
+        ],
+        chunkIds: "deterministic",    //生成id的算法 natural: 使用自然数(不推荐),named: 使用包所在目录作为name(在开发环境推荐),deterministic: 生成id, 针对相同文件生成的id是不变
+        splitChunks: {
+            chunks: "all",// async异步导入 initial同步导入 all 异步/同步导入
+            minSize: 20000,// 最小尺寸: 如果拆分出来一个, 那么拆分出来的这个包的大小最小为minSize，否则不拆分
+            maxSize: 20000,  // 将大于maxSize的包, 拆分成不小于minSize的包
+            minChunks: 1,// minChunks表示引入的包, 至少被导入几次才拆分
+            cacheGroups: {       //分组
+                // vendor: {
+                //     test: /[\\/]node_modules[\\/]/,   //匹配
+                //     //filename: "[id]_vendors.js",     //有占位的文件名
+                //     name: "vendor-chunks.js",    //文件名
+                //     priority: -10          //优先级
+                // },
+                bar: {
+                    test: /bar_/,
+                    filename: "[id]_bar.js"
+                },
+                default: {
+                    minChunks: 2,
+                    filename: "common_[id].js",
+                    priority: -20
+                }
+            }
+        },
+        //在运行环境中，对模块进行解析、加载、模块信息相关的runtime代码抽取到一个单独的chunk中
+        //runtimeChunk:true// true/multiple打包到多个包里面 single打包到一个包里面
+        runtimeChunk: {
+            name: function (entrypoint) {    //name属性决定runtimeChunk的名称；
+                return `why-${entrypoint.name}`
+            }
+        }
+    },
+
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: [
+                    isProduction ? 
+                    MiniCssExtractPlugin.loader: 
                     "style-loader",     //style-loader负责样式的插入
                     {
                         loader: "css-loader",   //css-loader负责css文件的解析
@@ -90,8 +139,8 @@ const commonConfig = {
             }
         ],
     },
+
     plugins: [
-        
         new HtmlWebpackPlugin({
             title: '测试',      //html文件的title
             template: "./index.html"   //html模板的路径
@@ -113,17 +162,13 @@ const commonConfig = {
             ]
         }),
         new VueLoaderPlugin(),  //加载vue文件
-        
     ]
-}
-module.exports = function(env) {
+})
+module.exports = function (env) {
     console.log(env)
     const isProduction = env.production;
-    process.env.NODE_ENV = isProduction ? "production": "development";
-  
+    process.env.NODE_ENV = isProduction ? "production" : "development";
     const config = isProduction ? prodConfig : devConfig;
-    const mergeConfig = merge(commonConfig, config);
-  
+    const mergeConfig = merge(commonConfig(isProduction), config);
     return mergeConfig;
-  };
-  
+};
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,10 +1,36 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const isProduction = true;
 
 module.exports = {
   mode: "production",
+  optimization: {
+    minimize: false,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,//使用多进程并发运行提高构建的速度，默认值是true，并发运行的默认数量： os.cpus().length - 1
+        extractComments: true,   //默认值true，会将注释抽取到一个单独的文件中
+        terserOptions: {
+          compress: {           //设置压缩相关的选项
+            arguments: false,
+            dead_code: true
+          },
+          mangle: true,       //丑化相关的选项，可以直接设置为true
+          toplevel: true,    //底层变量是否进行转换
+          keep_classnames: true,  //keep_classnames：保留类的名称
+          keep_fnames: true //keep_fnames：保留函数的名称
+        }
+      })
+    ]
+  },
   plugins: [
     // 生成环境
     new CleanWebpackPlugin(),   //打包时自动清除之前打包的内容
+    new MiniCssExtractPlugin({    //将css提取到一个独立的css文件中
+      filename: "css/[name].[hash:8].css"
+    }),
+    new CssMinimizerPlugin(),  //css压缩 去除无用的空格等
   ]
 }
